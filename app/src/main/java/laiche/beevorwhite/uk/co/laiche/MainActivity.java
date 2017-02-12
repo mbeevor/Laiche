801f237a9cec031a1a09217ef3ddd25ad27a3a7a
mbeevor <mbeevor@gmail.com>
2017-02-12 17:53:59 +0000

    With functioning web button

--- app/src/main/java/laiche/beevorwhite/uk/co/laiche/MainActivity.java
+++ app/src/main/java/laiche/beevorwhite/uk/co/laiche/MainActivity.java
@@ -1,5 +1,7 @@
 package laiche.beevorwhite.uk.co.laiche;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     public void openWebPage(View v) {
         Uri webpage = Uri.parse("http://www.YOUR-WEBSITE.com/");
