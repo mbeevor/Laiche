2dd2a4174031ff4425f3b1a8964866872cdb1cfc
mbeevor <mbeevor@gmail.com>
2017-04-25 14:47:20 +0100

    Clean code

--- app/src/main/java/laiche/beevorwhite/uk/co/laiche/MainActivity.java
+++ app/src/main/java/laiche/beevorwhite/uk/co/laiche/MainActivity.java
@@ -14,12 +14,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void openWebPage(View v) {
-        Uri webpage = Uri.parse("http://www.YOUR-WEBSITE.com/");
+    public void openWebPage(View view) {
+        Uri webpage = Uri.parse("http://www.beevorwhite.co.uk/");
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
     }
+
+    public void composeEmail(View view) {
+        Intent email = new Intent(Intent.ACTION_SENDTO);
+        email.setData(Uri.parse("mailto:hello@beevorwhite.co.uk"));
+        email.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email));
+        if (email.resolveActivity(getPackageManager()) != null) {
+            startActivity(email);
+        }
+    }
 }
+
+
 
